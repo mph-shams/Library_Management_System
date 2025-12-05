33d3ff53bc1f7de47024408f86a37be6dac01a1d
hossein2081 <117431730+hossein2081@users.noreply.github.com>
2025-12-05 16:50:04 +0330

    Edit DTO : RegisterRequestDto

--- LibraryManagement.Application/DTOs/RegisterRequestDto.cs
+++ LibraryManagement.Application/DTOs/RegisterRequestDto.cs
@@ -1,6 +1,25 @@
-﻿namespace Library_Management_System.LibraryManagement.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_Management_System.LibraryManagement.Application.DTOs
 {
     public class RegisterRequestDto
     {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string FullName { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "Member";
     }
 }
