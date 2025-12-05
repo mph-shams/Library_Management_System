911a19e5393cc6cc8f64a3d7339e36cbe99cdda7
hossein2081 <117431730+hossein2081@users.noreply.github.com>
2025-12-05 16:59:05 +0330

    Edit IServices : ITokenService

--- LibraryManagement.Application/IServices/ITokenService.cs
+++ LibraryManagement.Application/IServices/ITokenService.cs
@@ -1,6 +1,9 @@
-﻿namespace Library_Management_System.LibraryManagement.Application.IServices
+﻿using Library_Management_System.LibraryManagement.Core.Entities;
+
+namespace LibraryManagement.Application.IServices
 {
-    public class ITokenService
+    public interface ITokenService
     {
+        string GenerateToken(User user);
     }
 }
