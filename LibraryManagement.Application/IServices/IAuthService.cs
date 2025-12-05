0765242a727c40db0c589444696d39d6c87808bb
hossein2081 <117431730+hossein2081@users.noreply.github.com>
2025-12-05 16:57:41 +0330

    Edit IServices : IAuthService

--- LibraryManagement.Application/IServices/IAuthService.cs
+++ LibraryManagement.Application/IServices/IAuthService.cs
@@ -1,6 +1,13 @@
-﻿namespace Library_Management_System.LibraryManagement.Application.IServices
+﻿using Library_Management_System.LibraryManagement.Application.DTOs;
+
+namespace LibraryManagement.Application.IServices
 {
-    public class IAuthService
+    public interface IAuthService
     {
+        Task<LoginResponseDto> LoginAsync(LoginRequestDto request);
+        Task<UserDto> RegisterAsync(RegisterRequestDto request);
+        Task<UserDto?> GetCurrentUserAsync(int userId);
+        Task<UserDto?> GetUserByIdAsync(int userId);
+        Task<IEnumerable<UserDto>> GetAllUsersAsync();
     }
 }
